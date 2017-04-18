d19ca943efa89bdc02e3c879026b653067a0821d
André Sparvolli de Matos <andresparvolli@hotmail.com>
2017-04-18 01:46:40 -0300

    Versão 1.0 (igual ao livro)

---
+++ src/br/com/mvbos/lgj/base/Util.java
@@ -0,0 +1,36 @@
+package br.com.mvbos.lgj.base;
+
+public class Util {
+
+	public static boolean colide( Elemento a, Elemento b ) {
+		if ( !a.isAtivo() || !b.isAtivo() ) {
+			return false;
+		}
+
+		// Posição no Eixo X + Largura do Elemento A e B
+		final int plA = a.getPx() + a.getLargura();
+		final int plB = b.getPx() + b.getLargura();
+
+		// Posição no Eixo Y + altura do Elemento A e B
+		final int paA = a.getPy() + a.getAltura();
+		final int paB = b.getPy() + b.getAltura();
+
+		if ( plA > b.getPx() && a.getPx() < plB && paA > b.getPy() && a.getPy() < paB ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public static boolean colideX( Elemento a, Elemento b ) {
+		if ( !a.isAtivo() || !b.isAtivo() ) {
+			return false;
+		}
+
+		if ( a.getPx() + a.getLargura() >= b.getPx() && a.getPx() <= b.getPx() + b.getLargura() ) {
+			return true;
+		}
+
+		return false;
+	}
+}
